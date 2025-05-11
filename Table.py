afdc77d4817196e2b50944f91787b89df18c88b9
tigas0611 <tiago.inverneiroantunes@gmail.com>
2025-05-11 23:53:45 +0100

    Update Table.py

--- Table.py
+++ Table.py
@@ -4,7 +4,7 @@ Created on Fri May  2 13:44:01 2025
 Proj class Table 
 @author: tiago
 """
-
+import graphics as g
 class Table:
     def __init__(self,win):
-        self.table()
+        t=g.Rectangle(g.Point(),g.Point())
