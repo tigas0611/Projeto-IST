4e5ef106e15d1590c1dedffd9cadd55c551c3fe2
tigas0611 <tiago.inverneiroantunes@gmail.com>
2025-05-12 13:28:32 +0100

    Update Table.py

--- Table.py
+++ Table.py
@@ -5,6 +5,27 @@ Proj class Table
 @author: tiago
 """
 import graphics as g
+
+win=g.GrapghWin('planta', 600,800)
+f=open('salaxx.txt','r')
+
 class Table:
-    def __init__(self,win):
-        t=g.Rectangle(g.Point(),g.Point())
+    def __init__(self):
+        self.tables=[]
+        self.mesa()
+        
+    def mesa(self,line):
+        if line[0]=='table':
+            self.tables.append(g.Rectangle(g.Point(line[1],line[2]),
+                                           g.Point(line[3],line[4])))
+            self.tables.draw(win)
+            self.divisoria(line[3],line[4])
+    def divisoria(self,x,y):
+        self.divisória.append(g.Rectangle())
+table=Table()
+
+for line in f:
+    table
+    if line[0]=='parede':
+        paredes=g.Rectancle(g.Point(line[1],line[2]),g.Point(line[3],line[4]))
+        
