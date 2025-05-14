5a572a91adb76680b771048b5ea772863cb42656
tigas0611 <tiago.inverneiroantunes@gmail.com>
2025-05-14 15:17:36 +0100

    Quase finalizado/ melhorias

--- Table.py
+++ Table.py
@@ -10,75 +10,83 @@ win=g.GraphWin('planta', 600,800)
 f=open('salaxx.txt','r')
 
 class Table:
-    def __init__(self,values,win):
+    def __init__(self):
         self.group_tables=[]
-        self.tables(values)
-        self.draw_group(win)
-
-        
-    def tables(self,line):
-        if line[0]=='mesa':
-            for i in range(1,len(values)):
-                values[i]=int(values[i])
-            for j in range(2):
-                for k in range(3):
-                    self.group_tables.append(g.Rectangle(g.Point(line[1]+j*198,line[2]+k*84),
-                                                   g.Point(line[3]+j*198,line[4]+k*84)))
 
 
-class table_division():   
-    def __init__(self,values,win):
-        self.group_division=[]
-        self.division(values)
-        self.draw_group(win)
         
+    def Position(self,line):
+        if 'Table'in line:
+            value=line.strip().split()
+            self.group_tables.append(eval(value[1]))
+
+            #for i in range(1,len(values)):
+                #values[i]=int(values[i])
+            #for j in range(2):
+                #for k in range(3):
+                    #self.group_tables.append(g.Rectangle(g.Point(line[1]+j*198,line[2]+k*84),
+                                                   #g.Point(line[3]+j*198,line[4]+k*84)))
         
-    def division(self,line):
-        if line[0]=='divisória':
-            for i in range(1,len(values)):
-                values[i]=int(values[i])
-            for j in range(2): 
-                self.group_division.append(g.Rectangle(g.Point(line[1]+j*198,line[2]),
-                                               g.Point(line[3]+j*198,line[4])))
-class docking_station():
-    def __init(self,values):
-        self.group_docking=[]
-        self.docking(values)
-        self.draw_group()
-    
-    def docking(self,line):
-        if line[0]=='docking':
-            for i in range(1,len(values)):
-                values[i]=int(values[i])
-            for j in range(2): 
-                self.group_docking.append(g.Rectangle(g.Point(line[1],line[2]),
-                                               g.Point(line[3],line[4])))
-    def draw_group():
-        
-        
-        
-def planta(self,win):
+
+    def draw_group(self,win):
         for table in self.group_tables:
             table.draw(win)
-        for divisoria in self.group_division:
-            divisoria.draw(win)
             
-
-
-class Docking_station():
+class Table_Division():   
     def __init__(self):
-        self.base()
+        self.group_division=[]
+
         
-            
         
+    def Position(self,line):
+        if 'Division'in line:
+            value=line.split()
+            self.group_division.append(eval(value[1]))
+
+    def draw_group(self,win):    
+            for table in self.group_division:
+                table.draw(win)
+
+
+
+class Docking_Station():
+    def __init(self):
+        self.group_docking=[]
+    
+    def Position(self,line):
+        if 'Docking'in line:
+            value=line.strip().split()
+            self.group_tables.append(eval(value[1]))
+            #for i in range(1,len(values)):
+                #values[i]=int(values[i])
+            #for j in range(2): 
+                #self.group_docking.append(g.Rectangle(g.Point(line[1],line[2]),
+                                               #g.Point(line[3],line[4])))
+    
+    def draw_group(self,win):
+        if len(self.group_docking)!=0:    
+            for table in self.group_docking:
+                table.draw(win)        
+        
+        
+        
+table=Table()
+table_div=Table_Division()   
+docking=Docking_Station()         
 
 for line in f:
-    values=line.split()
-    table=Table(values,win)
-    if values[0]=='parede':
-        for i in range(1,len(values)):
-            values[i]=int(values[i])    
-        paredes=g.Rectangle(g.Point(values[1],values[2]),g.Point(values[3],values[4]))
-        paredes.draw(win)
+    if 'Parede'in line:
+        value=line.split()
+        parede=eval(value[1])    
+        parede.draw(win)
+    table.Position(line)
+    table_div.Position(line)
+    #docking.Position(line)
     
-    print(table.group_tables)
+table.draw_group(win)
+table_div.draw_group(win)
+#docking.draw_group(win)
+
+
+
+
