13f565b13a0fb07dae9cce71ddfd671d3d78de59
tigas0611 <tiago.inverneiroantunes@gmail.com>
2025-05-19 21:31:07 +0100

    tier0

--- Table.py
+++ Table.py
@@ -15,13 +15,16 @@ class Table:
 
 
         
-    def Position(self,rows,dist,nºtable,):
-        for i in range(rows):
-            for j in range(div):
+    def Position(self,scale,nºrows,distable,nºtable,tablesizex,tablesizey,divgap,divsize,distdiv,nºdivrows,tablegapx,tablegapy,Xdiv):
+        for i in range(nºrows):
+            for j in range(nºdivrows):
                 for k in range(2):
-                    self.group_tables.append(g.Rectangle(g.Point(distable+,),
-                                                         g.Point(,)))
-                    
+                    for l in range(nºtable):
+                        self.group_tables.append(g.Rectangle(g.Point(scale*(distable + k*(sizex+2*divgap + divsize)+ i*(tablegapx+sizex)),
+                                                                     scale*(2*distdiv + j*(2*Xdiv+nºtable*(sizey+tablegapy)-tablegapy) +l*(sizey+tablegapy))),
+                                                         g.Point(scale*(sizex + distable + k*(sizex+2*divgap + divsize)+ i*(tablegapx+sizex)),
+                                                                 scale*(sizey + 2*distdiv + j*(2*Xdiv+nºtable*(sizey+tablegapy)-tablegapy) +l*(sizey+tablegapy)))))
+        print(self.group_tables)
 
 
     def draw_group(self,win):
@@ -70,80 +73,81 @@ class Docking_Station():
 table=Table()
 table_div=Table_Division()   
 docking=Docking_Station()         
-
+#indiferença o nome da variavel values ou qualquer outro intermedio ser igual pois usamos só para aceder a valores dentro dela
 for line in f:
     if 'Tamanho das mesas' in line:
         values=line.split(':')
-        medidas=values.split(':')
-        tablesizex=int(medidas[0])
+        medidas=values[1].split('x')
+        tablesizey=int(medidas[0])
         tablesizex=int(medidas[1])
     if 'Tamanho das divisórias' in line:
-        line.split(':')
-        divsize=int(line[1])
+        values=line.split(':')
+        divsize=int(values[1])
     
     if 'nº de mesas por divisoria' in line:
-        line.split(':')
-        tablediv=int(line[1])
+        values=line.split(':')
+        nºtable=int(values[1])
     
     if 'nº de divisórias por fila' in line:
-        line.split(':')
-        nºdivrows=int(line[1])
+        values=line.split(':')
+        nºdivrows=int(values[1])
         
     if 'nº de filas' in line:
-        line.split(':')
-        nºrows=int(line[1])
-        
-    if 'espaço entre filas' in line:
-        line.split(':')
-        rowsgap=int(line[1])
+        values=line.split(':')
+        nºrows=int(values[1])
         
     if 'espaço entre mesas' in line:
-        line.split(':')
-        tablegap=int(line[1])
+        values=line.split(':')
+        tablegap=values[1].split('x')
+        tablegapy=int(tablegap[0])
+        tablegapx=int(tablegap[1])
+            
         
     if 'espaço entre as mesas e as paredes' in line:
-        line.split(':')
-        distable=int(line[1])
+        values=line.split(':')
+        distable=int(values[1])
         
     if 'espaço entre as mesas e as divisórias' in line:
-        line.split(':')
-        divgap=int(line[1])
+        values=line.split(':')
+        divgap=int(values[1])
     
     if 'Tamanho da docking Station' in line:
-        line.split(':')
-        docking=int(line[1])
+        values=line.split(':')
+        docking=values[1].split('x')
+        dockingx=int(docking[1])
+        dockingy=int(docking[0])
     
     if 'espaço entre as paredes e a divisórias' in line:
-        line.split(':')
-        distdiv=int(line[1])
+        values=line.split(':')
+        distdiv=int(values[1])
     
     if 'extra parte da divisória' in line:
-        line.split(':')
-        Xdiv=int(line[1])
+        values=line.split(':')
+        Xdiv=int(values[1])
         
     if 'espaço entre divisórias' in line:
-        line.split(':')
-        splitdiv=int(line[1])
+        values=line.split(':')
+        splitdiv=int(values[1])
         
     if 'escala' in line:
-        line.split(':')
-        scale=int(line[1])
+        values=line.split(':')
+        scale=int(values[1])
          
-sizex=nºrows*(2*(tablesizex + divgap) + divsize) + (nºrows-1)*rowsgap + 2*(distable)
+sizex=nºrows*(2*(tablesizex + divgap) + divsize) + (nºrows-1)*tablegapx + 2*(distable)
 
-sizey=2*(distdiv) + 2*nºdivrows*Xdiv + (nºdivrows-1)*splitdiv + tablediv*tablesizey  
-+ (tablediv-1)*tablegap  
+sizey=2*(distdiv) + 2*nºdivrows*Xdiv + (nºdivrows-1)*splitdiv + nºtable*tablesizey  
++ (nºtable-1)*tablegap  
 
-win.setCoords(0, scale*sizey, scale*sizex, 0)
+#win.setCoords(0, scale*sizey, scale*sizex, 0)
         
         
-    table.Position(line)
-    table_div.Position(line)
-    docking.Position(line)
+table.Position(scale,nºrows,distable,nºtable,tablesizex,tablesizey,divgap,divsize,distdiv,nºdivrows,tablegapx,tablegapy,Xdiv)
+#table_div.Position(line)
+#docking.Position(line)
     
 table.draw_group(win)
-table_div.draw_group(win)
-docking.draw_group(win)
+#table_div.draw_group(win)
+#docking.draw_group(win)
 
 
 
