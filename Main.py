4c9d7ebcc716f368485495570945195498ae024b
Gabi9385 <gabriel.neto.escola@gmail.com>
2025-05-30 14:08:12 +0100

    custom window sizw

--- Main.py
+++ Main.py
@@ -16,15 +16,21 @@ for line in f:
     if line == None:
         continue
     
+    elif 'Window size' in line:
+        values = line.split(': ')
+        values2 = values[1].split(' x ')
+        windowroomsizeX = int(values2[0])
+        windowroomsizeY = int(values2[1])
+    
     elif 'Table size' in line:
         values = line.split(': ')
         values2 = values[1].split(' x ')
-        tablesizeX = int(values2[0])
-        tablesizeY = int(values2[1])
+        tableroomsizeX = int(values2[0])
+        tableroomsizeY = int(values2[1])
         
     elif 'Divider width' in line:
         values = line.split(': ')
-        dividersizeX = int(values[1])
+        dividerroomsizeX = int(values[1])
     
     elif 'Number of tables per divisory' in line:
         values = line.split(': ')
@@ -62,7 +68,7 @@ for line in f:
     
     elif 'Divider extra size' in line:
         values = line.split(': ')
-        dividerextrasizeY = int(values[1])
+        dividerextraroomsizeY = int(values[1])
         
     elif 'Gap between dividers (horizontal)' in line:
         values = line.split(': ')
@@ -74,31 +80,31 @@ for line in f:
     
 f.close()
 
-sizeX = 2*(tablewallgapX + tablesizeX + tabledividergapX) + (numrows - 1)*dividergapX + dividersizeX
-dividersizeY = 2*dividerextrasizeY + numtables*(tablesizeY + tablegapY) - tablegapY
-sizeY = 2*(dividerwallgapY) + numdividers*(dividergapY + dividersizeY) - dividergapY
+roomsizeX = 2*(tablewallgapX + tableroomsizeX + tabledividergapX) + (numrows - 1)*dividergapX + dividerroomsizeX
+dividerroomsizeY = 2*dividerextraroomsizeY + numtables*(tableroomsizeY + tablegapY) - tablegapY
+roomsizeY = 2*(dividerwallgapY) + numdividers*(dividergapY + dividerroomsizeY) - dividergapY
 
-tableoffsetX = tablesizeX + 2*tabledividergapX + dividersizeX
-tableoffsetY = tablegapY + tablesizeY
+tableoffsetX = tableroomsizeX + 2*tabledividergapX + dividerroomsizeX
+tableoffsetY = tablegapY + tableroomsizeY
 
-divideroffsetY = dividersizeY + dividergapY
+divideroffsetY = dividerroomsizeY + dividergapY
 
 table = sa.Table()
 divider = sa.Divider()   
 platedelivery = sa.PlateDelivery()
 
-table.Position(numrows,tablewallgapX,numtables,tablesizeX,tablesizeY,dividerwallgapY,numdividers,dividerextrasizeY,tableoffsetX,dividergapX,tableoffsetY,divideroffsetY)
-divider.Position(numrows,numdividers,tablewallgapX,tablesizeX,dividergapX,dividergapY,dividersizeX,dividerwallgapY,divideroffsetY,tabledividergapX,dividersizeY)
-platedelivery.Position(sizeX,platedeliveryx,platedeliveryy)
+table.Position(numrows,tablewallgapX,numtables,tableroomsizeX,tableroomsizeY,dividerwallgapY,numdividers,dividerextraroomsizeY,tableoffsetX,dividergapX,tableoffsetY,divideroffsetY)
+divider.Position(numrows,numdividers,tablewallgapX,tableroomsizeX,dividergapX,dividergapY,dividerroomsizeX,dividerwallgapY,divideroffsetY,tabledividergapX,dividerroomsizeY)
+platedelivery.Position(roomsizeX,platedeliveryx,platedeliveryy)
 
-win = gr.GraphWin('Planta da Sala', 800,600)
+win = gr.GraphWin('Planta da Sala', windowroomsizeX, windowroomsizeY)
 
 table.draw_group(win)
 divider.draw_group(win)
 platedelivery.draw_group(win)
 
 quitbutton = qb.QuitButton(win, gr.Point(1, 1), gr.Point(12, 9), 'Quit')
-robot = wa.Waiter(win, gr.Point((sizeX + platedeliveryx)/2 + 4, platedeliveryy/2), 5)
+robot = wa.Waiter(win, gr.Point((roomsizeX + platedeliveryx)/2 + 4, platedeliveryy/2), 5)
 
 close = False
 while close is False:
@@ -106,6 +112,6 @@ while close is False:
     if quitbutton.pressed(mouseclick) is True:
         close = True
     else:
-        robot.pathfinding(table.grouptables, tablewallgapX, tablesizeX, tabledividergapX, dividerwallgapY, dividergapX, dividersizeX, platedeliveryy, numrows, sizeX)
+        robot.pathfinding(table.grouptables, tablewallgapX, tableroomsizeX, tabledividergapX, dividerwallgapY, dividergapX, dividerroomsizeX, platedeliveryy, numrows, roomsizeX)
     
 win.close()
